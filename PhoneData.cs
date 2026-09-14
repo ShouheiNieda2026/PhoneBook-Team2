4e9972fe6eb9219a038639739535ff8eedbd6e37
ShouheiNieda2026 <uni202607_16@telework.to>
2026-09-14 11:10:43 +0900

    PhoneDataクラスとdata.txt追加

---
+++ PhoneData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook_Team2
+{
+    class PhoneData
+    {
+        public string Name { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+    }
+}
